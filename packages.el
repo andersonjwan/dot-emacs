a9b4461b13de48391606b2d5de1906c27d1ebd10
Jacob Anderson <andersonjwan@gmail.com>
2022-06-20 15:47:33 -0400

    feat: Add base packages configuration

---
+++ packages.el
@@ -0,0 +1,30 @@
+;;; packages.el --- Package configurations for Emacs -*- lexical-binding: t; -*-
+
+;;; Copyright (C) 2022 Jacob Anderson
+
+;; Author: Jacob Anderson <andersonjwan@gmail.com>
+;; Created: 20 Jun 2022
+;; Keywords: packages, dot-emacs, configuration, custom
+;; Homepage: https://github.com/andersonjwan/.emacs.d
+
+;;; Package Management
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+;; Update package registry
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Install base packages
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;;; Packages
+
+;;; packages.el ends here
