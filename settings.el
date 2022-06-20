ac99238585d028ed0867ba8dbfb829b597d9b7d7
Jacob Anderson <andersonjwan@gmail.com>
2022-06-20 16:39:38 -0400

    feat: Add backup settings

--- settings.el
+++ settings.el
@@ -13,4 +13,25 @@
 (menu-bar-mode 0)   ; hide the menu bar
 (scroll-bar-mode 0) ; hide the scroll bar
 
+;;; Backup Settings
+
+;; Create and set backup path
+(defvar settings--backup-path (concat user-emacs-directory "backups"))
+(unless (file-directory-p settings--backup-path)
+  (make-directory settings-backup-path t))
+
+(setq backup-directory-alist `(("." . ,settings--backup-path)))
+
+;; Set backup configurations
+(setq make-backup-files t           ; make backup files
+      backup-by-copying t           ; copy file to backup directory
+      version-control t             ; maintain versions of the same file
+      delete-old-versions t         ; remove older backups
+      delete-by-moving-to-trash nil ; send files to system trash
+      kept-old-versions 8           ; old versions to keep
+      kept-new-versions 12          ; new versions to keep
+      auto-save-default t           ; auto-save files
+      auto-save-timeout 30          ; save buffers after t idle seconds
+      auto-save-interval 500)       ; save buffers after n keystrokes
+
 ;;; settings.el ends here
