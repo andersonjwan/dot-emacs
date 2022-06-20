de2b85b381e077cd0cabab52947a283190d76f82
Jacob Anderson <andersonjwan@gmail.com>
2022-06-20 15:00:49 -0400

    chore: Create base initialization file

---
+++ init.el
@@ -0,0 +1,10 @@
+;;; init.el --- Initialization file for Emacs -*- lexical-binding: t; -*-
+
+;;; Copyright (C) 2022 Jacob Anderson
+
+;; Author: Jacob Anderson <andersonjwan@gmail.com>
+;; Created: 20 Jun 2022
+;; Keywords: dot-emacs, configuration, custom
+;; Homepage: https://github.com/andersonjwan/.emacs.d
+
+;;; init.el ends here
