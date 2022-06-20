2c143464e6ef7732bb6a494fcd6066b7d509c4d1
Jacob Anderson <andersonjwan@gmail.com>
2022-06-20 16:11:23 -0400

    feat: Add general Emacs customizations file

---
+++ custom.el
@@ -0,0 +1,15 @@
+;;; custom.el --- General customizations for Emacs -*- lexical-binding: t; -*-
+
+;;; Copyright (C) 2022 Jacob Anderson
+
+;; Author: Jacob Anderson <andersonjwan@gmail.com>
+;; Created: 20 Jun 2022
+;; Keywords: customization, dot-emacs, configuration, custom
+;; Homepage: https://github.com/andersonjwan/.emacs.d
+
+;;; X-Emacs Customizations
+(tool-bar-mode 0)   ; hide the tool bar
+(menu-bar-mode 0)   ; hide the menu bar
+(scroll-bar-mode 0) ; hide the scroll bar
+
+;;; custom.el ends here
