00666cba88567c1df8addd855f21974e8f8e9e6d
Jacob Anderson <andersonjwan@gmail.com>
2022-06-21 11:53:30 -0400

    feat: Load appearance

--- init.el
+++ init.el
@@ -7,8 +7,16 @@
 ;; Keywords: initialization, dot-emacs, configuration, custom
 ;; Homepage: https://github.com/andersonjwan/.emacs.d
 
+;;; Commentary:
+;;
+;; This file contains initialization configurations for
+;; my own custom Emacs installation.
+
+;;; Code:
+
 ;; Load configuration
 (load (concat user-emacs-directory "settings.el"))
 (load (concat user-emacs-directory "packages.el"))
+(load (concat user-emacs-directory "appearance.el"))
 
 ;;; init.el ends here
