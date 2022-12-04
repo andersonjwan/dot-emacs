8caac857d2e0b9973e60885aba7070f1801d7a74
Jacob Anderson <andersonjwan@gmail.com>
2022-12-03 20:21:30 -0700

    feat: Add clang-format+

--- packages/prog/cpp.el
+++ packages/prog/cpp.el
@@ -16,6 +16,20 @@
 
 ;;; Package Management
 (use-package clang-format
+  ;;; Support code formatting using `clang-format'.
+  ;;;
+  ;;; For more information, see
+  ;;; https://github.com/llvm-mirror/clang/blob/master/tools/clang-format/clang-format.el
+  :ensure t)
+
+(use-package clang-format+
+  ;;; Support feature niceties for `clang-format'.
+  ;;;
+  ;;; Most importantly, this provides a nice minor mode (`clang-format+-mode')
+  ;;; that enables on-save formatting configurations.
+  ;;;
+  ;;; For more information, see
+  ;;; https://github.com/SavchenkoValeriy/emacs-clang-format-plus
   :ensure t)
 
 ;;; cpp.el ends here
