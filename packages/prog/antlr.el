1b5f09591e8deb23f7871630caaab6ebe26b4178
Jacob Anderson <andersonjwan@gmail.com>
2022-12-15 21:54:50 -0700

    feat: Add antlr configurations

---
+++ packages/prog/antlr.el
@@ -0,0 +1,27 @@
+;;; antlr.el --- ANTLR package configurations for Emacs -*- lexical-binding: t; -*-
+
+;;; Copyright (C) 2022 Jacob Anderson
+
+;; Author: Jacob Anderson <andersonjwan@gmail.com>
+;; Created: 15 December 2022
+;; Keywords: packages, dot-emacs, configuration, custom, antlr
+;; Homepage: https://github.com/andersonjwan/.emacs.d
+
+;;; Commentary:
+;;
+;; This file contains package configurations for my own custom
+;; Emacs installation specific to the ANTLR meta-language.
+
+;;; Code:
+
+;;; Package Management
+
+(use-package antlr
+  ;; Support ANTLR editing.
+  ;;
+  ;; For more information, see
+  ;; https://antlr-mode.sourceforge.net/
+  :ensure t
+  :mode ("\\.g4\\'" . antlr-mode))
+
+;;; antlr.el ends here
