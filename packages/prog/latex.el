1127e8eb38ffbdb2da2ec7a930235cb7c06d40fa
Jacob Anderson <andersonjwan@gmail.com>
2023-03-04 22:31:05 -0700

    feat: Support cleveref ref macros

--- packages/prog/latex.el
+++ packages/prog/latex.el
@@ -43,8 +43,24 @@
   ;;
   ;; This additionally insert the label automatically whenever the environment is
   ;; created through `auctex' by `reftek'.
-  :hook (LaTeX-mode . (lambda () (LaTeX-add-environments '("definition" LaTeX-env-label))
-			(add-to-list 'LaTeX-label-alist '("definition" . "def:"))))
+  :hook ((LaTeX-mode . (lambda () (LaTeX-add-environments '("definition" LaTeX-env-label))
+			 (add-to-list 'LaTeX-label-alist '("definition" . "def:"))))
+
+	 ;; Add the `cleveref' package macros to `reftex' referencing style list.
+	 ;;
+	 ;; This additional adds it to the TeX symbols, so these references may
+	 ;; also then be referred back to by `reftex'.
+	 (LaTeX-mode . (lambda ()
+			 (if (boundp 'reftex-ref-style-alist)
+			     (add-to-list 'reftex-ref-style-alist
+					  '("Cleveref" "cleveref"
+					    (("\\cref" ?c) ("\\Cref" ?C) ("\\cpageref" ?d) ("\\Cpageref" ?D)))))
+			 (reftex-ref-style-activate "Cleveref")
+			 (TeX-add-symbols
+			  '("cref" TeX-arg-ref)
+			  '("Cref" TeX-arg-ref)
+			  '("cpageref" TeX-arg-ref)
+			  '("Cpageref" TeX-arg-ref)))))
   :custom
   ;; This setting configures additional settings from AUCTeX that are set for
   ;; many popular packages (i.e., an easy configuration).
